c9aaaa795cbe355661a6605b1b2689dfba8d1d96
Amanda <amandaoliveirapolari@gmail.com>
2023-02-09 13:12:55 -0300

    Exercício 3 -> ok!

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,30 @@
-//Crie a const para a frase aqui
+// 📌 a)
+const frase = 'Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS, mas não deixe o gato sair"';
+console.log(frase);
+
+// 📌 b)
+function trocarPalavras(fraseInicial) {
+    const fraseFinal = fraseInicial.replace('verde','rosa').replace('azul','laranja');
+    console.log(fraseFinal);
+    return fraseFinal
+}
+// trocarPalavras(frase);
+const novaString = trocarPalavras(frase);
+// console.log(typeof novaString);
+
+// Nota: Se eu apenas pedir para a função escrever no console a fraseFinal, a minha 'novaString' fica com o tipo indefinido, agora, se eu pedir para ela retornar a 'fraseFinal' e passar como argumento para ela trabalhar a minha 'frase', funciona.
+
+
+// 📌 c)
+function verificarCores(string) {
+    const possuiVerde = string.includes('verde');
+    const possuiLaranja = string.includes('laranja');
+    const coresFinais = `
+    A frase inclui a cor verde? ${possuiVerde};
+    A frase inclui a cor laranja? ${possuiLaranja};
+    `
+    console.log(coresFinais)
+    return coresFinais
+}
+const resultadoCoresTrocadas = verificarCores(novaString);
+
